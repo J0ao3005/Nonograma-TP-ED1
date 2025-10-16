b843cb8ec934da507f92a12e9db218baee1b5986
J0ao3005 <142702876+J0ao3005@users.noreply.github.com>
2025-10-16 18:42:34 -0300

    Add files via upload

---
+++ Tp2/domino.h
@@ -0,0 +1,44 @@
+#ifndef DOMINO_H
+#define DOMINO_H
+
+#include <stdbool.h>
+//Representacao da peca(Domino)
+typedef struct {
+    int ladoA;
+    int ladoB;
+} Domino;
+
+//Celula
+typedef struct cell {
+    struct cell *prox;
+    Domino domino;
+} Celula;
+
+//Sentinela
+typedef struct {
+    Celula *pPrimeiro, *pUltimo;
+} Lista;
+
+//Leitura e inicializacao
+bool LeituraDomino(); //Realiza a leitura da entrada
+Lista *DominoCria(); //Inicializa a cabeça
+
+//Verificacoes e impressao
+bool listaEhVazia(Lista *); //Retorna se a lista eh vazia
+void DominoImprime(Lista *, bool, int); //Imprime  a lista
+void DominoImprimeLista(Lista *);
+
+//Resolucao do Problema
+bool DominoEhPossivelOrganizar(Lista *, int, int);
+bool DominoEhPossivelOrganizarRec(Lista *, Lista *, bool *, int, int); 
+bool encaixa(Domino, Domino);
+
+//Manipulacoes da Lista
+bool DominoAdicionaPecaInicio(Lista *, Domino); //Adiciona no inicio
+bool DominoAdicionaPecaFinal(Lista *, Domino); // Adiciona no Final
+bool DominoRemoveInicio(Lista *); //Remove no inicio  
+bool removeFinal(Lista *); //Remove final
+void DominoDestroi(Lista *); //Desaloca totalmente
+void invertePilha(Lista *, Lista *); //inverte a pilha
+
+#endif //DOMINO_H
